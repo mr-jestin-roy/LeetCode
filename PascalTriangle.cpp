98c32a3011a9e77c4aa3cb3972def9f965832bef
Nami2012 <namithasadeeshbabu@gmail.com>
2019-10-29 02:57:29 +0530

    Fixed Pascal's Triangle

--- PascalTriangle.cpp
+++ PascalTriangle.cpp
@@ -15,14 +15,19 @@ using namespace std;
             for(int k=0;k<pascal[i-1].size();k++)
             prev_row.push_back(pascal[i-1][k]);
             row.push_back(1);
-            for(int j=0;j<i;j++)
+            for(int j=1;j<i;j++)
                 row.push_back(prev_row[j]+prev_row[j-1]);
             row.push_back(1);
+            for(int k=0;k<row.size();k++)
+            cout<<row[k];
+            cout<<endl;
+            
             pascal.push_back(row);
         }
         for(int i=0;i<pascal.size();i++)
-        	for(int j=0;j<pascal[i].size();j++)
-        	  cout<<pascal[i][j];
+        	{for(int j=0;j<pascal[i].size();j++)
+        	  {cout<<pascal[i][j];}
+        	  cout<<endl;}
     }
     
 int main()
